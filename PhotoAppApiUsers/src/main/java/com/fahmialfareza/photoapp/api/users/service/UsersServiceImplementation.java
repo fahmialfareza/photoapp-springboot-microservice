b9a8fb8c8ea508657c7791f5c6e4ad871c427958
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-27 20:40:11 +0700

    Microservices Communication

--- PhotoAppApiUsers/src/main/java/com/fahmialfareza/photoapp/api/users/service/UsersServiceImplementation.java
+++ PhotoAppApiUsers/src/main/java/com/fahmialfareza/photoapp/api/users/service/UsersServiceImplementation.java
@@ -1,67 +1,101 @@
 package com.fahmialfareza.photoapp.api.users.service;
 
-import java.util.ArrayList;
-import java.util.UUID;
-
+import com.fahmialfareza.photoapp.api.users.data.AlbumsServiceClient;
+import com.fahmialfareza.photoapp.api.users.data.UserEntity;
+import com.fahmialfareza.photoapp.api.users.data.UsersRepository;
+import com.fahmialfareza.photoapp.api.users.shared.UserDto;
+import com.fahmialfareza.photoapp.api.users.ui.model.AlbumResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import com.fahmialfareza.photoapp.api.users.data.UserEntity;
-import com.fahmialfareza.photoapp.api.users.data.UsersRepository;
-import com.fahmialfareza.photoapp.api.users.shared.UserDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UsersServiceImplementation implements UsersService {
 
-	UsersRepository userRepository;
-	BCryptPasswordEncoder bCryptPasswordEncoder;
+    UsersRepository userRepository;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    // RestTemplate restTemplate;
+    Environment environment;
+    AlbumsServiceClient albumsServiceClient;
 
-	public UsersServiceImplementation(UsersRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.userRepository = userRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Override
-	public UserDto createUser(UserDto userDetails) {
-		// TODO Auto-generated method stub
-		userDetails.setUserId(UUID.randomUUID().toString());
-		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    public UsersServiceImplementation(UsersRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, RestTemplate restTemplate, Environment environment, AlbumsServiceClient albumsServiceClient) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        // this.restTemplate = restTemplate;
+        this.environment = environment;
+        this.albumsServiceClient = albumsServiceClient;
+    }
 
-		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        // TODO Auto-generated method stub
+        userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
-		userRepository.save(userEntity);
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
 
-		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+        userRepository.save(userEntity);
 
-		return returnValue;
-	}
+        UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		UserEntity userEntity = userRepository.findByEmail(username);
+        return returnValue;
+    }
 
-		if (userEntity == null)
-			throw new UsernameNotFoundException(username);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
 
-		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true,
-				new ArrayList<>());
-	}
+        if (userEntity == null) throw new UsernameNotFoundException(username);
 
-	@Override
-	public UserDto getUserDetailsByEmail(String email) {
-		UserEntity userEntity = userRepository.findByEmail(email);
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true, new ArrayList<>());
+    }
 
-		if (userEntity == null)
-			throw new UsernameNotFoundException(email);
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
 
-		return new ModelMapper().map(userEntity, UserDto.class);
-	}
+        if (userEntity == null) throw new UsernameNotFoundException(email);
+
+        return new ModelMapper().map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId);
+
+        if (userEntity == null) throw new UsernameNotFoundException(userId);
+
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+
+        /*
+        String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
+
+        ResponseEntity<List<AlbumResponseModel>> albumListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
+        });
+        List<AlbumResponseModel> albumsList = albumListResponse.getBody();
+        */
+
+        List<AlbumResponseModel> albumsList = albumsServiceClient.getAlbums(userId);
+
+        userDto.setAlbums(albumsList);
+
+        return userDto;
+    }
 
 }
